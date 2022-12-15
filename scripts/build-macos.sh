816207e9c5cf45e0349b5b121bbee75feb233295
Gaurav Verma <gaurav.verma@safetyculture.io>
2022-12-15 11:32:29 +1100

    INTG-2555 - Initial commit

---
+++ scripts/build-macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH=${PATH}:`go env GOPATH`/bin
+echo "building on AMD64"
+wails build -platform darwin/amd64 -clean
+
+echo "Zipping Package"
+ditto -c -k --keepParent ./build/bin/safetyculture-exporter.app ./build/bin/macOS/safetyculture-exporter-darwin-amd64.zip
+echo "Cleaning up"
+rm -rf ./build/bin/safetyculture-exporter.app
+
+echo "building on ARM64"
+wails build -platform darwin/arm64 -clean
+echo "Zipping Package"
+ditto -c -k --keepParent ./build/bin/safetyculture-exporter.app ./build/bin/macOS/safetyculture-exporter-darwin-arm64.zip
+echo "Cleaning up"
+rm -rf ./build/bin/safetyculture-exporter.app
