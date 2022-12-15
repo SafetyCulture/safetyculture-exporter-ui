d4d78875c11651744f5b7dc4108fbd7d1549b69a
Gaurav Verma <gaurav.verma@safetyculture.io>
2022-12-15 15:41:09 +1100

    added linux and macOS

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -3,6 +3,6 @@
 wails build -platform linux/amd64 -clean
 
 cd build/bin
-tar -czf safetyculture-exporter.tar.gz safetyculture-exporter
+tar -czf exporter.tar.gz safetyculture-exporter
 mkdir linux
-mv safetyculture-exporter.tar.gz linux/
+mv exporter.tar.gz linux/
