0f177a30466ffb15ef7ae703a9761ffc48601a0f
Mick Stanciu <mick.stanciu@safetyculture.io>
2023-01-11 16:13:12 +1100

    wip version

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo "DEBUGGING VARIABLES"
-echo "TAG> ${ github.ref_name }"
+echo "TAG: ${VERSION}"
 
-wails build -platform linux/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${github.ref_name}"
+wails build -platform linux/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
 
 tar -czf exporter-linux-amd64.tar.gz ./build/bin/safetyculture-exporter
