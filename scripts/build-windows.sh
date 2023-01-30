8178c6f54845d9a8e72099100e4282150bd70877
Mick Stanciu <mick.stanciu@safetyculture.io>
2023-01-30 11:57:37 +1100

    zip instead of tar.gz

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -5,4 +5,4 @@ echo "CGO ENABLED: ${CGO_ENABLED}"
 
 wails build -platform windows/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}" -skipbindings
 
-tar -czf exporter-windows-amd64.tar.gz ./build/bin/safetyculture-exporter.exe
+zip -r exporter-windows-amd64.zip ./build/bin/safetyculture-exporter.exe
