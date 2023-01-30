4291cb393bab6582022784bb4dd1e77c6597f2e4
Mick Stanciu <mick.stanciu@safetyculture.io>
2023-01-30 11:39:36 +1100

    CGO_ENABLED on windows

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -3,6 +3,6 @@
 echo "BUILDING WITH VERSION: ${VERSION}"
 echo "CGO ENABLED: ${CGO_ENABLED}"
 
-wails build -platform windows/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
+wails build -platform windows/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}" -skipbindings
 
 tar -czf exporter-windows-amd64.tar.gz ./build/bin/safetyculture-exporter.exe
