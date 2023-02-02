7bdb926c481c42e1b81597a3d5280a758f45f1eb
Mick Stanciu <mick.stanciu@safetyculture.io>
2023-02-02 13:04:28 +1100

    better folder structure

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -5,4 +5,4 @@ echo "CGO ENABLED: ${CGO_ENABLED}"
 
 wails build -platform windows/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}" -skipbindings
 
-zip -r exporter-windows-amd64.zip ./build/bin/safetyculture-exporter.exe
+zip -j exporter-windows-amd64.zip ./build/bin/safetyculture-exporter.exe
