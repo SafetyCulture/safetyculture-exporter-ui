fcdaf62cc06c6470dc7f8f6cab59e1c982ab45bf
HarryTranAU <harry.tran@safetyculture.io>
2023-02-09 15:59:48 +1100

    INTG-2655 export folder css changes (#64)

--- frontend/src/routes/Config.svelte
+++ frontend/src/routes/Config.svelte
@@ -397,7 +397,7 @@
 		<div class="db-error" on:click={removeOverlay} on:keydown={removeOverlay}>
 			<div>Error connecting to the database</div>
 			<div>Please ensure the database details are correct</div>
-			<div>Click <a on:click={removeOverlay} on:keydown={removeOverlay}>here</a> to go back</div>
+			<div>Click here to go back</div>
 		</div>
 	</Overlay>
 {/if}
@@ -475,11 +475,13 @@
 				</div>
 			{/if}
 
+            {#if selectedExportFormat != null && (selectedExportFormat.value === 'csv')}
 			<div class="label">Folder location</div>
-			<div id="folder" class="button-long selector border-weak border-round-8" on:click={openFolderDialog} on:keypress={openFolderDialog}>
-				<div class="text-weak" >{$shadowConfig["Export"]["Path"]}</div>
+			<div id="folder" class="button-long selector border-weak border-round-8 p-8 align-items-c" on:click={openFolderDialog} on:keypress={openFolderDialog}>
+				<div class="text-weak word-wrap-break width-18em" >{$shadowConfig["Export"]["Path"]}</div>
 				<img class="{build === 'windows' ? '' : 'cursor-pointer'}" src="../images/folder.png" alt="folder icon" width="15" height="15">
 			</div>
+            {/if}
 			{#if build === 'windows'}
 				<div class="sub-label m-top-4">To change folder location on Windows, move the executable to the new export folder</div>
 			{/if}
