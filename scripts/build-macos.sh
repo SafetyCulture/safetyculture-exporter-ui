5d1c5aa0ce756f1602521516b783a4f561c04638
HarryTranAU <58361130+HarryTranAU@users.noreply.github.com>
2023-02-16 10:41:31 +1100

    test arm build solo

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -3,13 +3,13 @@
 echo "BUILDING WITH VERSION: ${VERSION}"
 
 export PATH=${PATH}:`go env GOPATH`/bin
-echo "building on AMD64"
-wails build -platform darwin/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
-
-echo "Zipping Package"
-ditto -c -k --rsrc --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-amd64.zip
-echo "Cleaning up"
-rm -rf build/bin/SafetyCulture Exporter.app
+#echo "building on AMD64"
+#wails build -platform darwin/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
+#
+#echo "Zipping Package"
+#ditto -c -k --rsrc --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-amd64.zip
+#echo "Cleaning up"
+#rm -rf build/bin/safetyculture-exporter.app
 
 echo "building on ARM64"
 wails build -platform darwin/arm64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
@@ -17,4 +17,4 @@ wails build -platform darwin/arm64 -clean -ldflags "-s -w -X github.com/SafetyCu
 echo "Zipping Package"
 ditto -c -k --rsrc --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-arm64.zip
 echo "Cleaning up"
-rm -rf ./build/bin/SafetyCulture Exporter.app
+rm -rf ./build/bin/safetyculture-exporter.app
