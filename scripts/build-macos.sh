e4a12d7ba4604b6090f7df069bc0e5a3411a0cef
HarryTranAU <58361130+HarryTranAU@users.noreply.github.com>
2023-02-16 09:12:28 +1100

    change flags for mac zip

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -7,7 +7,7 @@ echo "building on AMD64"
 wails build -platform darwin/amd64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
 
 echo "Zipping Package"
-ditto -c -k --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-amd64.zip
+ditto -c -k --rsrc --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-amd64.zip
 echo "Cleaning up"
 rm -rf build/bin/SafetyCulture Exporter.app
 
@@ -15,6 +15,6 @@ echo "building on ARM64"
 wails build -platform darwin/arm64 -clean -ldflags "-s -w -X github.com/SafetyCulture/safetyculture-exporter-ui/internal/version.version=${VERSION}"
 
 echo "Zipping Package"
-ditto -c -k --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-arm64.zip
+ditto -c -k --rsrc --keepParent ./build/bin/safetyculture-exporter.app ./exporter-darwin-arm64.zip
 echo "Cleaning up"
 rm -rf ./build/bin/SafetyCulture Exporter.app
